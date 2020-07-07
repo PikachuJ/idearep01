afbf2d88faaa5c2ec6bb209fd5fbad6dec89ed6d
Pikachu <455110530@qq.com>
2020-07-07 09:54:34 +0800

    add 2 line code

--- src/main/java/com/anna/gittest/Hello.java
+++ src/main/java/com/anna/gittest/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git by idea");
         System.out.println("ad new code1");
+        System.out.println("this is the branch1");
+        System.out.println("master");
     }
 }
